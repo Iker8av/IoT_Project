b75b27a950dde82c527649c35772a4456ab289f9
Mar Bustos <marianabhdez@gmail.com>
2022-11-22 10:10:40 -0600

    avance carrusel

--- iot_project/src/components/Home/Home.jsx
+++ iot_project/src/components/Home/Home.jsx
@@ -11,7 +11,14 @@ export default function Home() {
             </div>
         </header>
         <section>
-          {/* Carrusel */}
+        <h2>House Status</h2>
+          <div class = "scrolling-wrapper">
+            <div class = 'card'><h2>Entries</h2></div>
+            <div class = 'card'><h2>Doors</h2></div>
+            <div class = 'card'><h2>Windows</h2></div>
+            <div class = 'card'><h2>Lights</h2></div>
+            <div class = 'card'><h2>Gas</h2></div>
+          </div>
         </section>
         {/* Sheet */}
     </div>
