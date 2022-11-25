06dc84137de0cebacd91414c052d580a42ef610a
Iker8av <iker8avv@gmail.com>
2022-11-25 10:06:27 -0600

    Acess Linked with Home

--- iot_project/src/components/Home/Home.jsx
+++ iot_project/src/components/Home/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Access from './Views/Access/Access'
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='Home'>
+        <header>
+            <div className='row'>
+                <h1>Welcome, Username!</h1> {/* User name prop */}
+                <Access/>
+            </div>
+        </header>
+        <section>
+          {/* Carrusel */}
+        </section>
+        {/* Sheet */}
+    </div>
   )
 }
